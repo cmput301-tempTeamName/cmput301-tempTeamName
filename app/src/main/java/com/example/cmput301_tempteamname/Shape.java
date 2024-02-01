6fbad2816cba4ef8db1f8004cedc52de4207effc
karlzhong2 <158379883+karlzhong2@users.noreply.github.com>
2024-01-31 23:14:02 -0700

    Update Shape.java

--- app/src/main/java/com/example/cmput301_tempteamname/Shape.java
+++ app/src/main/java/com/example/cmput301_tempteamname/Shape.java
@@ -1,9 +1,9 @@
 package com.example.cmput301_tempteamname;
 
 public abstract class Shape {
-    public abstract class Shape {
     int x;
     int y;
     String color = "blue"; // Add this line
-    }
+    
 }
+
