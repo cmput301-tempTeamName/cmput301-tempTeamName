d78f04d7fa462cf8a491d8472e939f3aa4cf86e9
karlzhong2 <158379883+karlzhong2@users.noreply.github.com>
2024-01-31 22:36:06 -0700

    Update Circle.java

---
+++ app/src/main/java/com/example/cmput301_tempteamname/Circle.java
@@ -0,0 +1,10 @@
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(int x, int y, double radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    
+}
